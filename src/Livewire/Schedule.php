4942f9d49d1b12a57eb45b4b4c2aee680d41389f
Hari-Krishnan <hari.k@in.flowglobal.net>
2025-03-20 12:14:27 +0530

    FEAT: [KAN-2912] - Laravel Pulse Schedule Monitoring Card

---
+++ src/Livewire/Schedule.php
@@ -0,0 +1,186 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hari\Laravel\Pulse\Schedule\Livewire;
+
+use function Safe\preg_replace;
+
+use Closure;
+use Cron\CronExpression;
+use DateTimeZone;
+use Illuminate\Console\Application;
+use Illuminate\Console\Scheduling\CallbackEvent;
+use Illuminate\Console\Scheduling\Event;
+use Illuminate\Console\Scheduling\Schedule as IlluminateSchedule;
+use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Carbon;
+use Laravel\Pulse\Livewire\Card;
+use Livewire\Attributes\Lazy;
+use ReflectionClass;
+use ReflectionFunction;
+use DB;
+
+#[Lazy]
+class Schedule extends Card
+{
+    #[Url(as: 'selectedcountry')]
+    public string $selectedcountry = 'ALL'; // Default to 'ALL' to show all
+
+    public function render(ConsoleKernel $kernel, IlluminateSchedule $schedule): View
+    {
+        $kernel->bootstrap();
+
+
+        $events = collect($schedule->events())->map(function (Event $event): array {
+
+            $command = $this->getCommand($event);
+
+            $taskresult = $this->getschedulerstatus($command);
+
+            $timezone = new DateTimeZone(session('timezone')?? 'UTC');
+
+            return [
+                'command' => $command,
+                'expression' => $this->getExpression($event),
+                'next_due' => $this->getNextDueDateForEvent($event, $timezone),
+                'status' => $taskresult ? $taskresult->status : null,
+                'failed_at' => $taskresult ?  $taskresult->last_failed_at :null,
+                'reason' => $taskresult ? $taskresult->failedlog : null
+            ];
+        });
+
+        // Filter the events based on the selected country
+        if ($this->selectedcountry !== 'ALL') {
+            $events = $events->filter(function ($event) {
+                return str_contains($event['command'], $this->selectedcountry);
+            });
+        }
+
+        return view('pulse-schedule::livewire.schedule', [
+            'events' => $events,
+        ]);
+    }
+
+    private function getClosureLocation(CallbackEvent $event): string
+    {
+        $callback = (new ReflectionClass($event))->getProperty('callback')->getValue($event);
+
+        if ($callback instanceof Closure) {
+            $function = new ReflectionFunction($callback);
+
+            return sprintf(
+                '%s:%s',
+                str_replace(app()->basePath().DIRECTORY_SEPARATOR, '', $function->getFileName() ?: ''),
+                $function->getStartLine()
+            );
+        }
+
+        if (is_string($callback)) {
+            return $callback;
+        }
+
+        if (is_array($callback)) {
+            $className = is_string($callback[0]) ? $callback[0] : $callback[0]::class;
+
+            return sprintf('%s::%s', $className, $callback[1]);
+        }
+
+        return sprintf('%s::__invoke', $callback::class); // @phpstan-ignore-line
+    }
+
+    private function getCommand(Event $event): string
+    {
+        $command = str_replace([Application::phpBinary(), Application::artisanBinary()], [
+            'php',
+            preg_replace("#['\"]#", '', Application::artisanBinary()),
+        ], $event->command ?? '');
+
+        if ($event instanceof CallbackEvent) {
+            $command = $event->getSummaryForDisplay();
+
+            if (in_array($command, ['Closure', 'Callback'], true)) {
+                $command = 'Closure at: '.$this->getClosureLocation($event);
+            }
+        }
+
+        return mb_strlen($command) > 1 ? "{$command} " : '';
+    }
+
+    private function getExpression(Event $event): string
+    {
+        if (! $event->isRepeatable()) {
+            return $event->getExpression();
+        }
+
+        return "{$event->getExpression()} ({$event->repeatSeconds}s)";
+    }
+
+    private function getNextDueDateForEvent(Event $event, DateTimeZone $timezone): Carbon
+    {
+        $nextDueDate = Carbon::instance(
+            (new CronExpression($event->expression))
+                ->getNextRunDate(Carbon::now()->setTimezone($event->timezone))
+                ->setTimezone($timezone)
+        );
+
+        if (! $event->isRepeatable()) {
+            return $nextDueDate;
+        }
+
+        $previousDueDate = Carbon::instance(
+            (new CronExpression($event->expression))
+                ->getPreviousRunDate(Carbon::now()->setTimezone($event->timezone), allowCurrentDate: true)
+                ->setTimezone($timezone)
+        );
+
+        $now = Carbon::now()->setTimezone($event->timezone)->startOfMinute();
+
+        if (! $now->copy()->startOfMinute()->eq($previousDueDate)) {
+            return $nextDueDate;
+        }
+
+        return $now
+            ->endOfSecond()
+            ->ceilSeconds($event->repeatSeconds); // @phpstan-ignore-line
+    }
+
+    private function getschedulerstatus($command)
+    {
+
+        $task = DB::table('monitored_scheduled_tasks')
+                ->where('name', $command)
+                ->select('id', 'last_started_at', 'last_finished_at', 'last_failed_at')
+                ->first();
+
+        $taskstatus = new \stdClass();
+
+        if(!empty($task)) {
+            if ($task->last_failed_at) {
+                $failedlog = DB::table('monitored_scheduled_task_log_items')
+                    ->where('monitored_scheduled_task_id', $task->id)
+                    ->where('type', 'failed')
+                    ->select('meta')
+                    ->orderBy('id', 'desc')
+                    ->first();
+            
+                $taskstatus->status = 'Failed';
+                $taskstatus->last_failed_at = $task->last_failed_at;
+                if ($failedlog && $failedlog->meta) {
+                    $failureMessage = json_decode($failedlog->meta, true);
+                    $taskstatus->failedlog = $failureMessage['failure_message'] ?? 'No message';
+                } else {
+                    $taskstatus->failedlog = 'Unknown'; 
+                }
+            }
+            else {
+                $taskstatus->status = 'Success';
+                $taskstatus->failedlog = null;
+                $taskstatus->last_failed_at = null;
+            }
+            return $taskstatus;
+        }
+
+    }
+}
